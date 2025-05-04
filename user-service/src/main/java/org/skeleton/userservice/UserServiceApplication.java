b27dfee55bc97774eb51db773040354a1ff8861b
perlowicz <bartek.perlinski1234@gmail.com>
2025-05-04 12:45:51 +0200

    add discovery-server

--- user-service/src/main/java/org/skeleton/userservice/UserServiceApplication.java
+++ user-service/src/main/java/org/skeleton/userservice/UserServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class UserServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 }
